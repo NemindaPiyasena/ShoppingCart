116ada04ff4a578da0f2be7703dec7024520e8f0
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-10-05 01:22:12 +0530

    moved the files of index.html to index_Euro_Flora.html and created a resposive navigation bar to the homepage

--- index.php
+++ index.php
@@ -5,14 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link href="main.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="styles/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <ul>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="signup.php">Sign Up</a></li>
-        <li><a href="blob.php">Upload image</a></li>
-    </ul>
+    <div class="navBar" id="navBarId">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="signup.php">Sign Up</a></li>
+            <li><a href="blob.php">Upload image</a></li>
+            <li class="icon"><a href="javascript:void();" onclick="navBarFunction();"><i class="fa fa-bars"></i></a></li>
+        </ul>
+    </div>
 
     <table class="product">
 
@@ -56,6 +61,9 @@
     
 
     </table>
+
+
+    <script src="scripts/index.js"></script>
 
 
 </body>
