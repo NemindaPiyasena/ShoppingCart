06858e571eb5a293c39c4aba8d4434c5d7a237f8
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-10-04 23:01:43 +0530

    Added a label

--- site.php
+++ site.php
@@ -3,9 +3,9 @@
 <body>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
+    <label>Select image to upload:</label>
+    <input type="file" name="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
 </form>
 
 </body>
