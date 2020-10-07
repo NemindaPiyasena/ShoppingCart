aa63c70e2eb8316ae1fa1cd3273075c70a27c00a
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-10-07 18:09:57 +0530

    changed the navBar

--- index.php
+++ index.php
@@ -5,17 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link href="main.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/index.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div class="navBar" id="navBarId">
         <ul>
-            <li><a href="index.php">Home</a></li>
+            <li class="icon"><a href="javascript:void();" onclick="navBarFunction();"><i class="fa fa-bars"></i></a></li>
+            
             <li><a href="login.php">Login</a></li>
             <li><a href="signup.php">Sign Up</a></li>
             <li><a href="blob.php">Upload image</a></li>
-            <li class="icon"><a href="javascript:void();" onclick="navBarFunction();"><i class="fa fa-bars"></i></a></li>
+            <li><a href="index.php">Home</a></li>
+            
         </ul>
     </div>
 
