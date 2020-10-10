b31c50c3a944a0cda7311133fbbe53b7e4582ee2
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-10-10 19:38:40 +0530

    merged html home page into php file

--- index.php
+++ index.php
@@ -4,28 +4,102 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link href="main.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="main.css?v = <?php echo time(); ?>" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="navBar" id="navBarId">
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="login.php">Login</a></li>
-            <li><a href="signup.php">Sign Up</a></li>
-            <li><a href="blob.php">Upload image</a></li>
-            <li class="icon"><a href="javascript:void();" onclick="navBarFunction();"><i class="fa fa-bars"></i></a></li>
-        </ul>
-    </div>
+    <section id="headerSec">
+        <img src="images/logo-small.png" alt="euroflora logo">
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.html" class="navcontainer">
+                        <i class="fa fa-home" aria-hidden="true" style="color: rgb(192, 92, 92);font-size: 20px;"></i>
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="contact.html" class="navcontainer">
+                        <i class="fa fa-envelope" aria-hidden="true" style="color: rgb(192, 92, 92);font-size: 20px;"></i>
+                        Contact us
+                    </a>
+                </li>
+                <li>
+                    <a href="index.html" class="navcontainer">
+                        <i class="fa fa-th-large" aria-hidden="true" style="color: rgb(192, 92, 92);font-size: 20px;"></i>
+                        The florist
+                    </a>
+                </li>
+                <li>
+                    <a href="index.html" class="navcontainer">
+                        <i class="fa fa-camera" aria-hidden="true" style="color: rgb(192, 92, 92);font-size: 20px;"></i>
+                        Gallery
+                    </a>
+                </li>
+                <li>
+                    <a href="index.html" class="navcontainer">
+                        <i class="fa fa-cogs" aria-hidden="true" style="color: rgb(192, 92, 92);font-size: 20px;"></i>
+                        How to order
+                    </a>
+                </li>
+                <li>
+                    <a href="index.html" class="navcontainer">
+                        <i class="fa fa-phone" aria-hidden="true" style="color: rgb(192, 92, 92);font-size: 20px;"></i>
+                        Order by phone
+                    </a>
+                </li>
+                <li>
+                    <a href="index.html" class="navcontainer">
+                        <i class="fa fa-comment" aria-hidden="true" style="color: rgb(192, 92, 92);font-size: 20px;"></i>
+                        Chat
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </section>
+
+    <article id="bodyForMain">
+        <article id="firstChild">
+            <section class="initialHeader">
+                <p><b>Malshan Flora</b> your online Florist to deliver Flowers in Kotikawatta</p>
+            </section>
+        
+            <section id="poster">
+                <div id="discriptImg">
+                    
+                    <div class="innerDiscripter" id="firstPoster">
+                        <h3>SEPTEMBER OFFERS: TODAY -15%</h3>
+                        <hr \>
+                        <p>Delivery service included</p>
+                    </div>
+                    <img src="images/euroflora-florist-flower-bouquet.jpg" alt="euroflora-florist-flower-bouquet" style="width: 100%;height: 100%;">
+                    
+                </div>
+                <div id="discountImg">
+
+                    <div id="secondPoster" class="innerDiscripter">
+                        <p>Bright gerberas or daisies.<br \>alstroemerias and mixed fowers<br \>from 78.80 €</p>
+                    </div>
+                    <div id="burst-12"><p>-5%</p></div>
+                    <img src="images/Cheyanne.jpg" alt="Cheyanne" style="width: 100%;height: 100%;">
+                </div>                
+            </section>
+
+            <section>
+                <p>
+                    We offer a same day Flowers and Plants delivery service in Kotikawatta even in a few hours. 
+                    From our online Flower shop can order nice Bouquets and Flowers Gifts quickly and easily. 
+                    Choose your Bouquet from 68 € delivery included. Payments accepted: Credit Card and Paypal.
+                </p>
+            </section>
 
     <table class="product">
 
         <?php 
         
             include 'database.php';
-            session_start();
-
             $query = "SELECT * FROM products";
             $statement = $connection->prepare($query);
             $statement->execute();
